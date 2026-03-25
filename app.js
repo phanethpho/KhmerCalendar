4ed7c0696198064d6e73f0f9fe05fbbab45b2055
PHANETH PHO <phaneth2014@gmail.com>
2026-03-25 13:37:05 +0700

    fix netlify functions directory name

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+
 import express from "express";
 import serverless from "serverless-http";
 
 const app = express();
 
 app.get("/hello", (req, res) => {
-  res.json({ message: "Hello from Express on Netlify serer" });
+  res.json({ message: "Hello from Express on Netlify" });
 });
 
 export const handler = serverless(app);
